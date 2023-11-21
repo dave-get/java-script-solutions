1ec2144b3c474be105fc0d8319fd94a6efd0a94c
dave-get <davewaanofii@gmail.com>
2023-11-20 21:42:58 -0900

    Day 1 introduction

---
+++ Day 1 introduction/day1.js
@@ -0,0 +1,21 @@
+var name;
+let age;
+var grade;
+let list;
+
+const Name = "dave";
+const date = "10/21/2023";
+const Age = 23;
+const country = "Ethiopia";
+
+let first_name, last_name, marital_status, Country, your_Age;
+
+const First_name = "dave",
+  Last_name = "get",
+  Marital_status = "none",
+  Your_Country = "Ethiopia",
+  Your_Age = 23;
+
+let myAge = 25;
+let yourAge = 30;
+console.log(`I am ${myAge} years old. \nYou are ${yourAge} years old.`);
